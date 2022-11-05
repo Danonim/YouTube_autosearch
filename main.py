ea9fce28f9141b1d900950ee8822e9b6a8ebae0a
Danonim <danonim373@gmail.com>
2022-11-05 13:27:00 +0200

    corrected

--- main.py
+++ main.py
@@ -8,19 +8,19 @@ title = input("Введіть назву відео: ")
 driver = webdriver.Chrome()
 
 def main():
-    driver.maximize_window()
-
-    driver.get("https://www.youtube.com")
-    driver.find_element(By.NAME, "search_query").send_keys(title)
-    driver.find_element(By.CSS_SELECTOR, "#search-icon-legacy.ytd-searchbox").click()
 
     try:
+        driver.maximize_window()
+        driver.get("https://www.youtube.com")
+        driver.find_element(By.NAME, "search_query").send_keys(title)
+        driver.find_element(By.CSS_SELECTOR, "#search-icon-legacy.ytd-searchbox").click()
+
         wait = WebDriverWait(driver, 100)
         wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="video-title"]/yt-formatted-string')))
+        driver.find_element(By.XPATH, '//*[@id="video-title"]/yt-formatted-string').click()
     except:
         print("Купи інтернет клоун")
 
-    driver.find_element(By.XPATH, '//*[@id="video-title"]/yt-formatted-string').click()
 
 if __name__ == "__main__":
     main()
